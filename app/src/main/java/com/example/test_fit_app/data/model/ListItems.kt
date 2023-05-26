ac2460a9854271520115abdb649aa075ad7d4505
George <96864675+gogi1704@users.noreply.github.com>
2023-05-26 17:22:39 +0300

    small fixes

--- app/src/main/java/com/example/test_fit_app/data/model/ListItems.kt
+++ app/src/main/java/com/example/test_fit_app/data/model/ListItems.kt
@@ -1,15 +1,13 @@
 package com.example.test_fit_app.data.model
 
 sealed class ListItems {
-    abstract val id: Int
+
 }
 
 data class LessonItem(
-    override val id: Int = -1,
     val lessonModel: LessonModel
 ) : ListItems()
 
 data class DateItem(
-    override val id: Int = -1,
     val date: String
 ) : ListItems()
